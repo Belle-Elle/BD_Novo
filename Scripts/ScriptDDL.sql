a9bb16e9995bab1c6b711973b421528aaebbbe6f
Isabelle Cristine Medeiros Amancio <40263753824@info.scs>
2022-05-30 17:19:38 -0300

    Alterado

--- Scripts/ScriptDDL.sql
+++ Scripts/ScriptDDL.sql
@@ -5,7 +5,7 @@ USE GerenciamentoCampanha
 GO
 
 CREATE TABLE usuario(
-id int  IDENTITY(1,1) NOT NULL,
+id int IDENTITY(1,1) NOT NULL,
 NIF VARCHAR (100) NOT NULL,
 senha VARCHAR (100) NOT NULL
 );
@@ -13,7 +13,12 @@ GO
 
 
 CREATE TABLE campanha(
-id int IDENTITY(1,1) NOT NULL,
+id  TINYINT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 imagem VARCHAR (250) NOT NULL
 );
 GO
+
+INSERT usuario (NIF, senha) VALUES ( '123456789', '123')
+INSERT usuario (NIF, senha) VALUES ( '987654321', '123')
+GO
+
--- Scripts/ScriptDML.sql
+++ Scripts/ScriptDML.sql
@@ -1,9 +1,7 @@
 USE GerenciamentoCampanha
 Go
 
-INSERT usuario (NIF, senha) VALUES ( '123456789', '123')
-INSERT usuario (NIF, senha) VALUES ( '987654321', '123')
-GO
+
 
 SELECT * FROM campanha
 GO
